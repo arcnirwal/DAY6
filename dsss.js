ee7f4936a4f779daf58a4e1ec79713aa6ec3b687
arcnirwal <154900947+arcnirwal@users.noreply.github.com>
2024-04-30 17:49:37 +0530

    Add files via upload

---
+++ dsss.js
@@ -0,0 +1,20 @@
+// console.log('gec start');
+// function Printpretty(){
+//     console.log(':ETEWTWETEWTWE');
+//     let ans=2+3;
+//     console.log(ans);
+//     console.log('preetyend');
+// }
+// setTimeout(Printpretty,10000);
+// console.log('gec end')
+// const a = document.getElementById('btn')
+// a.addEventListener('click',function(){
+//     console.log('button clicked');
+// })
+// console.log('start')
+// fetch("https//api.github.com/users/deepak3448");
+// console.log(req);
+// req.then(()=>console.log('fetched')
+//  const req = fetch('https://dummyjson.com/products/1')
+//          const res = req.then(res=>res.json())
+//          res.then((res)=>{console.log(res)})
---
+++ pos.js
@@ -0,0 +1,24 @@
+// let array =[2,10,21,32];
+// function printarray(elem){
+    
+//         console.log(':',elem);
+// }
+
+    
+//     function printarray(arr){
+//         for(leti=0;i<arr.length;i++){
+//             printpretty(arr(i));
+//         }
+//         arr.array.forEach(function printarray(elem){
+    
+//             console.log(':',elem);
+//         })
+    
+            
+//         }
+//         printarray(arr);
+function Printpretty(){
+    console.log(':ETEWTWETEWTWE');
+};
+setTimeout(Printpretty,10000);
+// setTimeout(Printpretty,1000)
